f97fb400cd857a19d506e8aab1b282025fae25a8
Zach <57845873+L0rd-Z-19@users.noreply.github.com>
2020-05-03 11:09:27 -0500

    Completed the bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -4,16 +4,12 @@ fetch("http://l0rd-z-19.github.io/jsonStorage/samples.json")
     })
     .then(function(json){
         samples = json["samples"];
-        console.log(samples[0]);
-        console.log("OTU {i}".replace("{i}",samples[0].otu_ids.slice(0,10)));
+        //left side bar chart
         var top = samples[0].otu_ids.slice(0,10);
         var labels = [];
         for(i = 0; i < 10; i++){
             labels.push("OTU {i}".replace("{i}",top[i]));
         }
-
-
-
         var barDrp = {
             x: samples[0].sample_values,
             transforms: [{
@@ -21,12 +17,23 @@ fetch("http://l0rd-z-19.github.io/jsonStorage/samples.json")
                 target: 'x',
                 order: 'ascending'}],
             y: labels,
+            text: samples[0].otu_labels,
             type: "bar",
             orientation:"h"
         };
         var data = [barDrp];
-        Plotly.newPlot("plot", data);
-          
+        Plotly.newPlot("left_bar", data);
+        
+        //Bubble chart
+        var bub = {
+            x: samples[0].otu_ids,
+            y: samples[0].sample_values,
+            text: samples[0].otu_labels,
+            mode:'markers',
+            marker:{size:samples[0].sample_values, color:samples[0].otu_ids}
+        };
+        var bubData = [bub];
+        Plotly.newPlot("bubbles", bubData);
 
 
 
