dcc4e878e1d26e32ab539d7df796b585da66d3cb
Zach <57845873+L0rd-Z-19@users.noreply.github.com>
2020-05-04 06:53:08 -0500

    finished the panel and dropdown so the page updates

--- static/js/app.js
+++ static/js/app.js
@@ -3,72 +3,67 @@ fetch("http://l0rd-z-19.github.io/jsonStorage/samples.json")
         return resp.json();
     })
     .then(function(json){
-        samples = json["samples"];
-        //left side bar chart
-        var top = samples[0].otu_ids.slice(0,10);
-        var labels = [];
-        for(i = 0; i < 10; i++){
-            labels.push("OTU {i}".replace("{i}",top[i]));
+        var metadata = json["metadata"];  
+        var samples = json["samples"];
+        var bellyBtn = 0;
+        //create a blank value as a starter for dropdown
+        selData = document.getElementById("selDataset");
+        opt = document.createElement("option");
+        opt.text = " ";
+        selData.add(opt);
+        //fill the dropdown values
+        for(i=0; i < 152;i++){
+            opt = document.createElement("option");
+            opt.text = metadata[i]["id"];
+            selData.add(opt);
         }
-        var barDrp = {
-            x: samples[0].sample_values,
-            transforms: [{
-                type: 'sort',
-                target: 'x',
-                order: 'ascending'}],
-            y: labels,
-            text: samples[0].otu_labels,
-            type: "bar",
-            orientation:"h"
-        };
-        var data = [barDrp];
-        Plotly.newPlot("left_bar", data);
-        
-        //Bubble chart
-        var bub = {
-            x: samples[0].otu_ids,
-            y: samples[0].sample_values,
-            text: samples[0].otu_labels,
-            mode:'markers',
-            marker:{size:samples[0].sample_values, color:samples[0].otu_ids}
-        };
-        var bubData = [bub];
-        Plotly.newPlot("bubbles", bubData);
+        //if the dropdown changes, update the page
+        selData.addEventListener("change",function(){
+            bellyBtn = selData.value - 940; 
+            
+            //left side bar chart
+            var top = samples[bellyBtn].otu_ids.slice(0,10);
+            var labels = [];
+            for(i = 0; i < 10; i++){
+                labels.push("OTU {i}".replace("{i}",top[i]));
+            }
+            var barDrp = {
+                x: samples[bellyBtn].sample_values,
+                transforms: [{
+                    type: 'sort',
+                    target: 'x',
+                    order: 'ascending'}],
+                y: labels,
+                text: samples[bellyBtn].otu_labels,
+                type: "bar",
+                orientation:"h"
+            };
+            var layout = {
+                width: 500
+            }
+            var data = [barDrp];
+            Plotly.newPlot("left_bar", data,layout);
+            
+            //Bubble chart
+            var bub = {
+                x: samples[bellyBtn].otu_ids,
+                y: samples[bellyBtn].sample_values,
+                text: samples[bellyBtn].otu_labels,
+                mode:'markers',
+                marker:{size:samples[bellyBtn].sample_values, color:samples[bellyBtn].otu_ids}
+            };
+            var bubData = [bub];
+            Plotly.newPlot("bubbles", bubData);
 
-        //metadata and demographic information
-        var metadata = json["metadata"];
-        console.log(metadata[0]);
-        var pan = d3.select("#sample-metadata");
-        for(i=0; i < 7;i++){
-            row = pan.append('h5');
-            Object.entries(metadata[0]).forEach(function([key,value]){
-                var tex = "{key}: {value}".replace("{key}", key)
-                var texf = tex.replace("{value}", value)
+            //metadata and demographic information
+            var pan = d3.select("#sample-metadata");
+            keys = ["id","ethnicity","gender","age","location","Beaufort/NC","bbtype","wfreq"];
+            for(i=0; i < 8;i++){
+                row = pan.append('h5');
+                var tex = "{key}: {value}".replace("{key}", keys[i])
+                var texf = tex.replace("{value}", metadata[bellyBtn][keys[i]])
                 row.text(texf);
-            });
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            }
+        });
 }); 
 
